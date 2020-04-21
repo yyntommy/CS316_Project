069414540c359d7699234fe810a78c0389358fc4
Ashley Murray <42747966+ashleyjmurray@users.noreply.github.com>
2020-04-21 01:33:39 -0400

    Update webscrapper.py
    update

--- webscrapper.py
+++ webscrapper.py
@@ -2,28 +2,26 @@
 
 import requests
 from bs4 import BeautifulSoup
+import csv
+import pandas as pd
 
 URLS = []
 starting = "https://www.roomies.com/profiles/"
 c = 500000
-counter = 500
+counter = 50
 text_data = []
 
 while (counter != 0):
     x_str = str(c)
     new_starting = starting + x_str
     page = requests.get(new_starting)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    mydivs = soup.body.div.findAll({'class':'markdown text-gray-darkest'})
-    text = ''
-    for x in mydivs:
-        text = x.find('p').text
-
-    text_data.append(text)
+    soup = BeautifulSoup(page.content, 'lxml')
+    container = soup.find("div",attrs={'class': 'markdown text-gray-darkest'})
+    for p in container.find_all('p'):
+        text_data.append(p.text)
+    
     c = c + 1
     counter = counter - 1
-    #print(x_str)
-
-with open('roommate_blog.txt', 'w') as filehandle:
-    for item in text_data:
-        filehandle.write('%s\n' % item)
+    
+df = pd.DataFrame(text_data)
+df.to_csv("output.csv")
